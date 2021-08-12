12d708df67008886eff5983d74063e43968a05b9
“TheLuisMeli” <ext_luboliva@mercadolibre.com.co>
2021-08-12 16:50:56 -0500

    uggly solution :/

--- src/com/co/pa/Main.java
+++ src/com/co/pa/Main.java
@@ -6,26 +6,19 @@ import java.util.Set;
 
 public class Main {
 
-    private static final int NUMBER_42 = 42;
-    private static final int MAX_IN = 10;
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Set<Integer> setIn = new HashSet<>();
-        Set<Integer> resultModuleSet = new HashSet<>();
+        Set<Integer> SetResult = new HashSet<>();
 
-        for (int i = 0; i < MAX_IN; i++){
+        for (int i = 0; i < 10; i++){
             setIn.add(scanner.nextInt());
         }
 
         setIn.stream().forEach((i) -> {
-            if(i < NUMBER_42) {
-                resultModuleSet.add(i);
-            } else{
-                resultModuleSet.add(i % NUMBER_42);
-            }
+            SetResult.add(i < 42 ? i : i % 42);
         });
 
-        System.out.println(resultModuleSet.size());
+        System.out.println(SetResult.size());
     }
 }
